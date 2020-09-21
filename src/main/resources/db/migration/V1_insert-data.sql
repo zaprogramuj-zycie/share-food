f5445ebf050cedb1b338ca4c7947c135e39d74fe
Mateusz Michalski <mateuszmichalski99@gmail.com>
2020-09-21 16:41:11 +0200

    T5 - Biblioteka flyway
    #Uruchomienie FlyWay

--- src/main/resources/db/migration/V1_insert-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO User values('Jan','test1@example.com');
-INSERT INTO User values('Adam','test2@example.com');
---
+++ src/main/resources/db/migration/V2__insert-data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user (id, name, email) values (0, 'Test 1','test1@example.com');
